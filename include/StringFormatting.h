0c83078493b450c12d1939b358c31673dbccf03b
Mrowr Purr <mrowr.purr@gmail.com>
2023-04-09 06:19:00 -0700

    Implement StringFormatting.h

--- include/StringFormatting.h
+++ include/StringFormatting.h
@@ -1,1 +1,9 @@
-int coolNumber() { return 42; }
+#pragma once
+
+#ifdef STRING_FORMATTING_USE_FMT
+#include <fmt/format.h>
+#define string_format(...) fmt::format(__VA_ARGS__)
+#else
+#include <format>
+#define string_format(...) std::format(__VA_ARGS__)
+#endif
