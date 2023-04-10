338a2e4d75d919af31540ac1df933ca8289813f4
Mrowr Purr <mrowr.purr@gmail.com>
2023-04-09 21:11:25 -0700

    Cleanup/simplify xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,11 @@
 add_rules("mode.debug")
 set_toolchains("msvc")
 set_languages("c++23")
+
+-- fmt is only required for the example
 add_requires("fmt")
 
+-- Enable fmt::format instead of std::format (for testing the example)
 option("use_fmt")
     add_defines("STRING_FORMATTING_USE_FMT")
     set_description("Enable fmt::format (instead of std::format)")
@@ -10,14 +13,13 @@ option("use_fmt")
     set_default(false)
 
 target("StringFormatting")
-    set_kind("library")
-    add_headerfiles("include/(**.h)", {prefixdir = "StringFormatting"})
+    set_kind("headeronly")
+    add_headerfiles("include/(**.h)")
     add_includedirs("include", {public = true})
 
 target("Example")
     set_kind("binary")
     add_files("Example.cpp")
     add_deps("StringFormatting")
-    add_imports("StringFormatting")
     add_packages("fmt")
     add_options("use_fmt")
