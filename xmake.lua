f5424a484e2daf12ff7617ba50c59a90998240eb
Mrowr Purr <mrowr.purr@gmail.com>
2023-04-26 17:51:28 -0700

    Update for string_format.h

--- xmake.lua
+++ xmake.lua
@@ -2,14 +2,14 @@ add_rules("mode.debug")
 set_toolchains("msvc")
 
 -- set to c++20 for <format> support
-set_languages("c++11")
+set_languages("c++14")
 
 -- fmt is only required for the example
 add_requires("fmt")
 
 target("string_format")
     set_kind("headeronly")
-    add_headerfiles("include/string_format")
+    add_headerfiles("include/(**.h)")
     add_includedirs("include", {public = true})
 
 target("Example")
