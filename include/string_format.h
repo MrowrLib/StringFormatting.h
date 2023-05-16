3c654c3c8e31fd283b8be45069759121e01eb06c
Mrowr Purr <mrowr.purr@gmail.com>
2023-05-15 19:57:24 -0700

    STRING_FORMAT_USE_FMT and STRING_FORMAT_USE_STD

--- include/string_format.h
+++ include/string_format.h
@@ -1,10 +1,12 @@
 #pragma once
 
-#if ((defined(_MSVC_LANG) && _MSVC_LANG >= 202002L) || (__cplusplus >= 202002L)) && \
-    __has_include(<format>)
+#if defined(STRING_FORMAT_USE_STD) ||                                                \
+    (!defined(STRING_FORMAT_USE_FMT) &&                                              \
+     ((defined(_MSVC_LANG) && _MSVC_LANG >= 202002L) || (__cplusplus >= 202002L)) && \
+     __has_include(<format>))
     #include <format>
     #define string_format(...) std::format(__VA_ARGS__)
-#elif __has_include(<fmt/format.h>)
+#elif defined(STRING_FORMAT_USE_FMT) || __has_include(<fmt/format.h>)
     #include <fmt/format.h>
     #define string_format(...) fmt::format(__VA_ARGS__)
 #else
